f01a0b0fbd61a2d892003a4d3f32d7703dc31089
Ricardo Andrés Carro <111796101+AndresCarro1@users.noreply.github.com>
2022-12-01 16:29:47 -0300

    Update Guess the Number.py

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,7 +1,7 @@
 # It's a guess the number game
 import random
 
-print ('Hi there! What is your name?')
+print ("Hi there! What's your name?")
 name = input ()
 
 print('Pleased to meet you '+ name + ', you will have to guess the number between 1 and 20. You can try 6 times.')
@@ -9,7 +9,7 @@ secretNumber = random.randint(1, 20)
 
 try:
     for attemptsMade in range (1, 7):
-        print ('What is the number?')
+        print ("What's the number?")
         attempt = int (input())
 
         if attempt < secretNumber:
