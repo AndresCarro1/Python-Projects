221ec2745b966a1124518d154a85514e3d418b49
Ricardo Andrés Carro <111796101+AndresCarro1@users.noreply.github.com>
2023-01-08 11:37:54 -0300

    Update PriceRetrieverML.py

--- PriceRetrieverML.py
+++ PriceRetrieverML.py
@@ -1,12 +1,19 @@
 import bs4, requests
 
-def getMercadoLibrePrice(productUrl):
-    res = requests.get(productUrl)
-    res.raise_for_status()
+print ("Please paste the article's URL from Mercado Libre.")
+article = input()
 
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#price > div > div.ui-pdp-price__second-line > span > span.andes-money-amount__fraction')
-    return elems[0].text.strip()
+try:
+    def getMercadoLibrePrice(productUrl):
+        res = requests.get(productUrl)
+        res.raise_for_status()
 
-price = getMercadoLibrePrice ('https://articulo.mercadolibre.com.ar/MLA-1155297868-mouse-inalambrico-logitech-g-series-lightspeed-g603-negro-_JM?variation=175069697653#reco_item_pos=2&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-trend-recommendations&reco_id=311bb00a-288e-4576-bfd3-59177c6ad84b&c_id=/home/navigation-trends-recommendations/element&c_element_order=3&c_uid=79b3d9ae-51c2-453a-9054-6b3e9dda22ad')
-print('The price is $' + price)                        
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
+        elems = soup.select('#price > div > div.ui-pdp-price__second-line > span > span.andes-money-amount__fraction')
+        return elems[0].text.strip()
+
+    price = getMercadoLibrePrice (article)
+    print('The price is $' + price)                        
+
+except IndexError:
+    print ("Sorry, could not find the product. Please try again later.")      
